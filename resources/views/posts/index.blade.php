ad6b60f2d78eeefef5567a08a81b5f584808c9bd
inomoto-aki <aki140323@outlook.jp>
2025-04-15 19:28:39 +0900

    refactor

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <titl>投稿一覧</titl>
     @if(session('flash_message'))
-    <p>{{ session('flash_message') }}</p>
+     <p>{{ session('flash_message') }}</p>
     @endif
   </head>
   <body>
@@ -27,27 +27,33 @@
     <main>
       <h1>投稿一覧</h1>
       @if (session('flash_message'))
-            <p>{{ session('flash_message') }}</p>
-        @endif
+          <p>{{ session('flash_message') }}</p>
+      @endif
 
         @if(session('error_message'))
-        <p>{{ session('error_message')}}</p>
+           <p>{{ session('error_message')}}</p>
         @endif
 
         <a href="{{ route('posts.create') }}">新規投稿</a>
 
       @if($posts->isNotEmpty())
         @foreach($posts as $post)
-        <article>
-          <h2>{{ $post->title }}</h2>
-          <p>{{$post->content }}</p>
-          <a href="{{ route('posts.show',$post) }}">詳細</a>
-          <a href="{{ route('posts.edit',$post) }}">編集</a>
-        </article>
-        @endforeach
+           <article>
+             <h2>{{ $post->title }}</h2>
+             <p>{{$post->content }}</p>
+             <a href="{{ route('posts.show',$post) }}">詳細</a>
+             <a href="{{ route('posts.edit',$post) }}">編集</a>
+  
+              <form action="{{ route('posts.destroy', $post) }}" method="POST" onsubmit="return confirm('本当に削除してもよろしいですか？');">
+                 @csrf
+                 @method('DELETE')
+                 <button type="submit">削除</button>
+               </form>
+           </article>
+         @endforeach
         @else
-        <p>投稿はありません。</p>
-        @endif
+            <p>投稿はありません。</p>
+       @endif
     </main>
 
     <footer>
